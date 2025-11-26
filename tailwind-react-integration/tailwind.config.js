4c1adc945cf569eddd84739c23eae129fc119827
Elaabdi Hafsa <hafsaaelaabdi@gmail.com>
2025-11-26 16:25:51 +0100

    Update Tailwind CSS configuration for React

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,16 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
-module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false,
-  theme: { extend: {} },
-  variants: { extend: {} },
-  plugins: [],
-};
